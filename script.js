7f0f29e673665950b8540cb1aa66598d5c36abb8
sarat kannan <kannansarat9@gmail.com>
2023-10-20 03:43:59 +0530

    add post call request

--- script.js
+++ script.js
@@ -6,6 +6,10 @@
 
         const statusInput = document.getElementById('status');
 
+        let initialMedicine;
+
+        let intensifiedMedicine;
+
         function addField() {
             const inputFields = document.getElementById("inputFields");
       
@@ -43,7 +47,16 @@
       
             
             // Log the values in the 2D array for demonstration
-            console.log(tempArray);
+            //console.log(tempArray);
+
+            for(let i = 0; i < tempArray.length; i++)
+            {
+                if(tempArray[i][0] === "initial"){
+                    initialMedicine.push([tempArray[i][1], tempArray[i][2]])
+                } else if(tempArray[i][0] === "intensified"){
+                    intensifiedMedicine.push([tempArray[i][1], tempArray[i][2]])
+                }
+            }
       
             // You can send the valuesArray to the server or further process it as needed
           }
@@ -52,42 +65,40 @@
           document.getElementById("myForm").addEventListener("submit", function (event) {
             storeValues();
 
-            
+            const name = nameInput.value;
+            const favourites = favouritesInput.value;
+            const status = statusInput.value;
 
+            // Define the data to send
+            const data = {
+                "nickName" : name,
+                "personalLike" : favourites, 
+                "criticality" : status, 
+                "initialMedicine" : initialMedicine, 
+                "intensifiedMedicine" : intensifiedMedicine
+            };
 
+            fetch('https://us-central1-adherer-3.cloudfunctions.net/app/updatePerson', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // 'Accept' : '*/*',
+                    // 'Accept-Encoding' : 'gzip, deflate, br',
+                    // 'Connection' : 'keep-alive'
+                    // You may need to include additional headers as required by the API
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(result => {
+                // Handle the response from the API here
+                console.log(result);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
 
-            //     const name = nameInput.value;
-
-        //     // Define the data to send
-        //     const data = {
-        //         "nickName" : name,
-        //         "personalLike" : "football", 
-        //         "criticality" : "intensified", 
-        //         "initialMedicine" : [["g", "1"]], 
-        //         "intensifiedMedicine" : [["h", "3"]]
-        //     };
-
-        //     fetch('https://us-central1-adherer-3.cloudfunctions.net/app/updatePerson', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             // 'Accept' : '*/*',
-        //             // 'Accept-Encoding' : 'gzip, deflate, br',
-        //             // 'Connection' : 'keep-alive'
-        //             // You may need to include additional headers as required by the API
-        //         },
-        //         body: JSON.stringify(data)
-        //     })
-        //     .then(response => response.json())
-        //     .then(result => {
-        //         // Handle the response from the API here
-        //         console.log(result);
-        //     })
-        //     .catch(error => {
-        //         console.error('Error:', error);
-        //     });
-
-            event.preventDefault(); // Prevent actual form submission
+            //event.preventDefault(); // Prevent actual form submission
           });
 
         // submitButton.addEventListener('click', () => {
